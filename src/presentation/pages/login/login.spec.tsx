c9c321bca9548fedad50f43401047835be24b9b6
Ariene Maiara <ariene.maiara@hotmail.com>
2020-08-31 20:00:16 -0300

    test: ensure Login will not render spinner and error in start

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Login from './login'
+
+describe('Login Component', () => {
+  test('Should not render form status', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
+  })
+})
